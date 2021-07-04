8974408159c4d211d86257559389111da82ded75
Jacob Morrison-Gardiner <61953269+jakeymg@users.noreply.github.com>
2021-07-04 16:38:35 +1000

    States

---
+++ Assets/Scripts/States/WalkState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Animancer;
+
+public class WalkState : State
+{
+    Player owner;
+    public WalkState(Player owner) { this.owner = owner;}
+
+    public override void OnEnter()
+    {
+        Debug.Log("Entering WalkState");
+        owner.GetComponent<PlayerAnimationManager>().PlayIdleAnimation();
+    }
+
+    public override void Execute()
+    {
+       
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exiting WalkState");
+    }
+}
