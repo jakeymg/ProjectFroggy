da4f08ecdf486902b2e9107fc86f8c252d14da16
Jacob Morrison-Gardiner <61953269+jakeymg@users.noreply.github.com>
2021-10-01 22:42:11 +1000

    SoftToonShaderDoneForNow

--- Assets/Shaders/Includes/Lighting.hlsl
+++ Assets/Shaders/Includes/Lighting.hlsl
@@ -23,4 +23,32 @@ void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 
     #endif
 }
 
+void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView, 
+    float MainDiffuse, float MainSpecular, float3 MainColor,
+    out float Diffuse, out float Specular, out float3 Color)
+{
+    Diffuse = MainDiffuse;
+    Specular = MainSpecular;
+    Color = MainColor * (MainDiffuse + MainSpecular);
+
+#ifndef SHADERGRAPH_PREVIEW
+    int pixelLightCount = GetAdditionalLightsCount();
+    for (int i=0; i < pixelLightCount; ++i)
+    {
+        Light light = GetAdditionalLight(i, WorldPosition);
+        half NdotL = saturate(dot(WorldNormal, light.direction));
+        half atten = light.distanceAttenuation * light.shadowAttenuation;
+        half thisDiffuse = atten * NdotL;
+        half thisSpecular = LightingSpecular(thisDiffuse, light.direction, WorldNormal, WorldView, 1, Smoothness);
+        Diffuse += thisDiffuse;
+        Specular += thisSpecular;
+        Color += light.color * (thisDiffuse + thisSpecular);
+    }
+#endif
+
+    half total = Diffuse + Specular;
+    // if no light touches this pixel, set the coor to the main light's color
+    Color = total <= 0 ? MainColor : Color / total;
+}
+
 #endif
